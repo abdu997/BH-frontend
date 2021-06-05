1fac01d1928de464f0067b06a7a16faff107fa1d
Abdul Amoud <aamou094@uottawa.ca>
2021-06-04 22:00:29 -0400

    Formatted Navbar file and changed its title

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,15 +1,15 @@
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
 
 export default function NavBar() {
     return (
         <AppBar position="static">
             <Toolbar variant="dense">
                 <Typography variant="h6" color="inherit">
-                    Sir Vote Alot
+                    Sir Vote-a-lot
                 </Typography>
             </Toolbar>
         </AppBar>
-    )
+    );
 }
