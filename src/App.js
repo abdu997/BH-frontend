5322f0256a7115b9a96422507b31260e2f766216
Abdul Amoud <aamou094@uottawa.ca>
2021-06-04 22:21:54 -0400

    Removed paper from app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
 import Container from "@material-ui/core/Container";
 
