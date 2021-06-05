3671cfcf47813dcd8b46c1fe567492347ae183ec
Abdul Amoud <aamou094@uottawa.ca>
2021-06-04 20:24:59 -0400

    Created poll form

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Container from "@material-ui/core/Container";
 
 import NavBar from "./components/NavBar";
 import CreatePollForm from "./components/CreatePollForm";
+import PollForm from "./components/PollForm";
 
 import PollContext from "./utils/PollContext";
 
@@ -33,6 +34,13 @@ function App() {
         setPollSettings({ pollQuestion, pollOptions });
     };
 
+    const registerVote = (option) => {
+        const poll = { ...pollSettings };
+        poll["pollOptions"][option]["voteCount"] =
+            pollSettings["pollOptions"][option]["voteCount"] + 1;
+        setPollSettings(poll);
+    };
+
     return (
         <PollContext.Provider value={pollSettings}>
             <NavBar />
@@ -47,7 +55,9 @@ function App() {
                             </Paper>
                         </Grid>
                         <Grid item md={4}>
-                            <Paper className={classes.paper}></Paper>
+                            <Paper className={classes.paper}>
+                                <PollForm registerVote={registerVote} />
+                            </Paper>
                         </Grid>
                         <Grid item md={4}>
                             <Paper className={classes.paper}></Paper>
